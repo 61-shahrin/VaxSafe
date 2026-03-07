613f11ec7024935803fa06e3153ca5935c479cb9
Sadia Shahrin Sharna <shahrin999910@gmail.com>
2026-03-07 01:05:53 -0800

    Add files via upload

---
+++ vaxsafe/views.py
@@ -0,0 +1,80 @@
+# vaxsafe/views.py
+# vaxsafe/views.py
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse
+
+
+# =====================================
+# PUBLIC PAGES
+# =====================================
+def home(request):
+    return render(request, 'htmlpages/home.html')
+
+
+def features(request):
+    return render(request, 'htmlpages/features.html')
+
+
+def aboutUs(request):
+    return render(request, 'htmlpages/aboutUs.html')
+
+
+def contact(request):
+    return render(request, 'htmlpages/contact.html')
+
+
+def send_message(request):
+    if request.method == "POST":
+        return HttpResponse("Message sent successfully")
+    return HttpResponse("Send message page")
+
+
+# =====================================
+# AUTHENTICATION
+# =====================================
+def register(request):
+    return render(request, 'htmlpages/register.html')
+
+
+def verify(request):
+    return render(request, 'htmlpages/verify.html')
+
+
+def login_view(request):
+    return render(request, 'htmlpages/login.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
+
+
+def verify_email(request):
+    return render(request, 'htmlpages/verify.html')
+
+
+# =====================================
+#  Dashboard & PROFILE
+# =====================================
+def dashboard(request):
+    return render(request, 'htmlpages/dashboard.html')
+def profile_view(request):
+    return render(request, 'htmlpages/profile.html')
+
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('profile')   # login হলে profile page
+        else:
+            return HttpResponse("Invalid username or password")
+
+    return render(request, 'htmlpages/login.html')
